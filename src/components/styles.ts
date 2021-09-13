dfd67e8dc6e52ed86037f63c47d83dea3d8b2506
Nikita Zheleznov <nikita.zheleznov@x5.ru>
2021-09-13 22:54:22 +0300

    feat(styled): + typography css

---
+++ src/components/styles.ts
@@ -0,0 +1,87 @@
+import { css } from 'styled-components'
+
+/**
+ * MVP дизайн системы X5
+ *
+ * https://www.figma.com/file/iqcF49V6I1fbyA8c2Lv8RW/HR.-%D0%9F%D0%B5%D1%80%D0%B5%D0%BA%D1%80%D0%B5%D1%81%D1%82%D0%BE%D0%BA---%D0%A2%D0%B8%D0%BF%D0%BE%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D0%BA%D0%B0?node-id=0%3A1
+ */
+
+export const heading = {
+	H1: css`
+		font-family: Roboto;
+		font-style: normal;
+		font-weight: bold;
+		font-size: 32px;
+		line-height: 40px;
+	`,
+	H2: css`
+		font-family: Roboto;
+		font-style: normal;
+		font-weight: bold;
+		font-size: 24px;
+		line-height: 32px;
+	`,
+	H3: css`
+		font-family: Roboto;
+		font-style: normal;
+		font-weight: bold;
+		font-size: 20px;
+		line-height: 28px;
+	`,
+
+	H4: css`
+		font-family: Roboto;
+		font-style: normal;
+		font-weight: bold;
+		font-size: 18px;
+		line-height: 24px;
+	`,
+}
+
+export const medium = {
+	t1: css`
+		font-family: Roboto;
+		font-style: normal;
+		font-weight: 500;
+		font-size: 16px;
+		line-height: 24px;
+	`,
+	t2: css`
+		font-family: Roboto;
+		font-style: normal;
+		font-weight: 500;
+		font-size: 14px;
+		line-height: 20px;
+	`,
+	t3: css`
+		font-family: Roboto;
+		font-style: normal;
+		font-weight: 500;
+		font-size: 12px;
+		line-height: 16px;
+	`,
+}
+
+export const regular = {
+	t1: css`
+		font-family: Roboto;
+		font-style: normal;
+		font-weight: normal;
+		font-size: 16px;
+		line-height: 24px;
+	`,
+	t2: css`
+		font-family: Roboto;
+		font-style: normal;
+		font-weight: 500;
+		font-size: 14px;
+		line-height: 20px;
+	`,
+	t3: css`
+		font-family: Roboto;
+		font-style: normal;
+		font-weight: normal;
+		font-size: 12px;
+		line-height: 16px;
+	`,
+}
