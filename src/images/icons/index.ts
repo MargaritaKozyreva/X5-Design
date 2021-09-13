b7f2586797c9672fe4ad2ab4643f4ba986ee26e8
Nikita Zheleznov <nikita.zheleznov@x5.ru>
2021-09-13 22:55:41 +0300

    feat(icons): + svg. NOT WORKING. adding mock

---
+++ src/images/icons/index.ts
@@ -0,0 +1,284 @@
+/**
+ * Вот так экспортирует svg Create React App 2 из коробки, 
+ * но кто-то эджектнул и снес
+ * у меня лапки, не знаю как чинить
+ */
+
+// import { ReactComponent as add_in_circle } from './add_in_circle.svg'
+// import { ReactComponent as add_person } from './add_person.svg'
+// import { ReactComponent as add_text } from './add_text.svg'
+// import { ReactComponent as apple_fill } from './apple_fill.svg'
+// import { ReactComponent as apple } from './apple.svg'
+// import { ReactComponent as archive_arrow } from './archive_arrow.svg'
+// import { ReactComponent as archive } from './archive.svg'
+// import { ReactComponent as attach } from './attach.svg'
+// import { ReactComponent as calendar } from './calendar.svg'
+// import { ReactComponent as card_history } from './card_history.svg'
+// import { ReactComponent as change_camera } from './change_camera.svg'
+// import { ReactComponent as check_circle } from './check_circle.svg'
+// import { ReactComponent as check_person } from './check_person.svg'
+// import { ReactComponent as chev_down_in_circle } from './chev_down_in_circle.svg'
+// import { ReactComponent as chev_up_in_circle } from './chev_up_in_circle.svg'
+// import { ReactComponent as circle } from './circle.svg'
+// import { ReactComponent as coin } from './coin.svg'
+// import { ReactComponent as comment } from './comment.svg'
+// import { ReactComponent as loupe } from './loupe.svg'
+// import { ReactComponent as compl } from './compl.svg'
+// import { ReactComponent as copy_1 } from './copy_1.svg'
+// import { ReactComponent as crop } from './crop.svg'
+// import { ReactComponent as del } from './delete.svg'
+// import { ReactComponent as dislike_active } from './dislike_active.svg'
+// import { ReactComponent as dislike } from './dislike.svg'
+// import { ReactComponent as document_1 } from './document_1.svg'
+// import { ReactComponent as document_download } from './document_download.svg'
+// import { ReactComponent as document } from './document.svg'
+// import { ReactComponent as done } from './done.svg'
+// import { ReactComponent as down_in_circle } from './down_in_circle.svg'
+// import { ReactComponent as dwnld } from './dwnld.svg'
+// import { ReactComponent as edit } from './edit.svg'
+// import { ReactComponent as error_2 } from './error_2.svg'
+// import { ReactComponent as error } from './error.svg'
+// import { ReactComponent as exclamation } from './exclamation.svg'
+// import { ReactComponent as eye_crossed } from './eye_crossed.svg'
+// import { ReactComponent as eye } from './eye.svg'
+// import { ReactComponent as faceid } from './faceid.svg'
+// import { ReactComponent as fall } from './fall.svg'
+// import { ReactComponent as fingerprint } from './fingerprint.svg'
+// import { ReactComponent as fire } from './fire.svg'
+// import { ReactComponent as flash_auto } from './flash_auto.svg'
+// import { ReactComponent as flash_off } from './flash_off.svg'
+// import { ReactComponent as flash } from './flash.svg'
+// import { ReactComponent as follower } from './follower.svg'
+// import { ReactComponent as fwd_10 } from './fwd_10.svg'
+// import { ReactComponent as gift } from './gift.svg'
+// import { ReactComponent as growth } from './growth.svg'
+// import { ReactComponent as heart_active } from './heart_active.svg'
+// import { ReactComponent as heart } from './heart.svg'
+// import { ReactComponent as help } from './help.svg'
+// import { ReactComponent as info_24 } from './info_24.svg'
+// import { ReactComponent as leave } from './leave.svg'
+// import { ReactComponent as like_active } from './like_active.svg'
+// import { ReactComponent as like } from './like.svg'
+// import { ReactComponent as link_1 } from './link_1.svg'
+// import { ReactComponent as link } from './link.svg'
+// import { ReactComponent as lock } from './lock.svg'
+// import { ReactComponent as mail } from './mail.svg'
+// import { ReactComponent as megaphone } from './megaphone.svg'
+// import { ReactComponent as message_1 } from './message_1.svg'
+// import { ReactComponent as message } from './message.svg'
+// import { ReactComponent as new_chat } from './new_chat.svg'
+// import { ReactComponent as next_track } from './next_track.svg'
+// import { ReactComponent as notifications_new_2 } from './notifications_new_2.svg'
+// import { ReactComponent as notifications_new_3 } from './notifications_new_3.svg'
+// import { ReactComponent as notifications_new } from './notifications_new.svg'
+// import { ReactComponent as notifications_with_counter } from './notifications_with_counter.svg'
+// import { ReactComponent as outside_the_box_3 } from './outside_the_box_3.svg'
+// import { ReactComponent as outside_the_box_4 } from './outside_the_box_4.svg'
+// import { ReactComponent as outside_the_box } from './outside_the_box.svg'
+// import { ReactComponent as pause } from './pause.svg'
+// import { ReactComponent as phone } from './phone.svg'
+// import { ReactComponent as play } from './play.svg'
+// import { ReactComponent as poll_done } from './poll_done.svg'
+// import { ReactComponent as poll_public_1 } from './poll_public_1.svg'
+// import { ReactComponent as poll_public_2 } from './poll_public_2.svg'
+// import { ReactComponent as poll_public } from './poll_public.svg'
+// import { ReactComponent as prev_track } from './prev_track.svg'
+// import { ReactComponent as profile } from './profile.svg'
+// import { ReactComponent as question_24 } from './question_24.svg'
+// import { ReactComponent as remove_person } from './remove_person.svg'
+// import { ReactComponent as rwd_10 } from './rwd_10.svg'
+// import { ReactComponent as save } from './save.svg'
+// import { ReactComponent as scan } from './scan.svg'
+// import { ReactComponent as sets } from './sets.svg'
+// import { ReactComponent as share } from './share.svg'
+// import { ReactComponent as shield } from './shield.svg'
+// import { ReactComponent as sound_off_16 } from './sound_off_16.svg'
+// import { ReactComponent as sound_off } from './sound_off.svg'
+// import { ReactComponent as sound_on_16 } from './sound_on_16.svg'
+// import { ReactComponent as sound_on } from './sound_on.svg'
+// import { ReactComponent as star_1 } from './star_1.svg'
+// import { ReactComponent as star_2 } from './star_2.svg'
+// import { ReactComponent as star } from './star.svg'
+// import { ReactComponent as study } from './study.svg'
+// import { ReactComponent as time_out } from './time_out.svg'
+// import { ReactComponent as time } from './time.svg'
+// import { ReactComponent as tree } from './tree.svg'
+// import { ReactComponent as write } from './write.svg'
+// import { ReactComponent as add } from './add.svg'
+// import { ReactComponent as arrow_down } from './arrow_down.svg'
+// import { ReactComponent as arrow_exchange } from './arrow_exchange.svg'
+// import { ReactComponent as arrow_left } from './arrow_left.svg'
+// import { ReactComponent as arrow_right } from './arrow_right.svg'
+// import { ReactComponent as arrow_up } from './arrow_up.svg'
+// import { ReactComponent as chevron_double_left } from './chevron_double_left.svg'
+// import { ReactComponent as chevron_double_right } from './chevron_double_right.svg'
+// import { ReactComponent as chevron_down } from './chevron_down.svg'
+// import { ReactComponent as chevron_left } from './chevron_left.svg'
+// import { ReactComponent as chevron_right } from './chevron_right.svg'
+// import { ReactComponent as chevron_up } from './chevron_up.svg'
+// import { ReactComponent as close } from './close.svg'
+// import { ReactComponent as download } from './download.svg'
+// import { ReactComponent as drag } from './drag.svg'
+// import { ReactComponent as more } from './more.svg'
+// import { ReactComponent as reply } from './reply.svg'
+// import { ReactComponent as upload } from './upload.svg'
+// import { ReactComponent as smile } from './smile.svg'
+// import { ReactComponent as pin } from './pin.svg'
+// import { ReactComponent as search_big } from './search_big.svg'
+// import { ReactComponent as add_image } from './add_image.svg'
+// import { ReactComponent as add_video } from './add_video.svg'
+// import { ReactComponent as photocamera1 } from './photocamera1.svg'
+// import { ReactComponent as photocamera2 } from './photocamera2.svg'
+// import { ReactComponent as photocamera_turnaround } from './photocamera_turnaround.svg'
+// import { ReactComponent as photocamera } from './photocamera.svg'
+// import { ReactComponent as takephoto } from './takephoto.svg'
+// import { ReactComponent as video_cam } from './video_cam.svg'
+// import { ReactComponent as video } from './video.svg'
+
+import Placeholder from "./Placeholder";
+
+/**
+ *
+ * WARNING!!! если у значка не меняется fill, то в .svg нужно ручками удалить fill у всех <path />
+ * тк. фигма экспортировала с захардкоженным цветом
+ *
+ * имена файлов преимущественно соответствуют именам компонентам фигмы
+ * https://www.figma.com/file/pf5qTeroLN3eyiMZijZPTy/%D0%9F%D0%B5%D1%80%D1%87%D0%B0%D1%82%D0%BA%D0%B0.-UI-Kit?node-id=12475%3A15140
+ */
+
+
+
+export const icon = {
+	add_in_circle: Placeholder,
+	add_person: Placeholder,
+	add_text: Placeholder,
+	apple_fill: Placeholder,
+	apple: Placeholder,
+	archive_arrow: Placeholder,
+	archive: Placeholder,
+	attach: Placeholder,
+	calendar: Placeholder,
+	card_history: Placeholder,
+	change_camera: Placeholder,
+	check_circle: Placeholder,
+	check_person: Placeholder,
+	chev_down_in_circle: Placeholder,
+	chev_up_in_circle: Placeholder,
+	circle: Placeholder,
+	coin: Placeholder,
+	comment: Placeholder,
+	compl: Placeholder,
+	copy_1: Placeholder,
+	crop: Placeholder,
+	del: Placeholder,
+	dislike_active: Placeholder,
+	dislike: Placeholder,
+	document_1: Placeholder,
+	document_download: Placeholder,
+	document: Placeholder,
+	done: Placeholder,
+	down_in_circle: Placeholder,
+	dwnld: Placeholder,
+	edit: Placeholder,
+	error_2: Placeholder,
+	error: Placeholder,
+	exclamation: Placeholder,
+	eye_crossed: Placeholder,
+	eye: Placeholder,
+	faceid: Placeholder,
+	fall: Placeholder,
+	fingerprint: Placeholder,
+	fire: Placeholder,
+	flash_auto: Placeholder,
+	flash_off: Placeholder,
+	flash: Placeholder,
+	follower: Placeholder,
+	fwd_10: Placeholder,
+	loupe: Placeholder,
+	gift: Placeholder,
+	growth: Placeholder,
+	heart_active: Placeholder,
+	heart: Placeholder,
+	help: Placeholder,
+	info_24: Placeholder,
+	leave: Placeholder,
+	like_active: Placeholder,
+	like: Placeholder,
+	link_1: Placeholder,
+	link: Placeholder,
+	lock: Placeholder,
+	mail: Placeholder,
+	megaphone: Placeholder,
+	message_1: Placeholder,
+	message: Placeholder,
+	new_chat: Placeholder,
+	next_track: Placeholder,
+	notifications_new_2: Placeholder,
+	notifications_new_3: Placeholder,
+	notifications_new: Placeholder,
+	notifications_with_counter: Placeholder,
+	outside_the_box_3: Placeholder,
+	outside_the_box_4: Placeholder,
+	outside_the_box: Placeholder,
+	pause: Placeholder,
+	phone: Placeholder,
+	play: Placeholder,
+	poll_done: Placeholder,
+	poll_public_1: Placeholder,
+	poll_public_2: Placeholder,
+	poll_public: Placeholder,
+	prev_track: Placeholder,
+	profile: Placeholder,
+	question_24: Placeholder,
+	remove_person: Placeholder,
+	rwd_10: Placeholder,
+	save: Placeholder,
+	scan: Placeholder,
+	sets: Placeholder,
+	share: Placeholder,
+	shield: Placeholder,
+	sound_off_16: Placeholder,
+	sound_off: Placeholder,
+	sound_on_16: Placeholder,
+	sound_on: Placeholder,
+	star_1: Placeholder,
+	star_2: Placeholder,
+	star: Placeholder,
+	study: Placeholder,
+	time_out: Placeholder,
+	time: Placeholder,
+	tree: Placeholder,
+	write: Placeholder,
+	add: Placeholder,
+	arrow_down: Placeholder,
+	arrow_exchange: Placeholder,
+	arrow_left: Placeholder,
+	arrow_right: Placeholder,
+	arrow_up: Placeholder,
+	chevron_double_left: Placeholder,
+	chevron_double_right: Placeholder,
+	chevron_down: Placeholder,
+	chevron_left: Placeholder,
+	chevron_right: Placeholder,
+	chevron_up: Placeholder,
+	close: Placeholder,
+	download: Placeholder,
+	drag: Placeholder,
+	more: Placeholder,
+	reply: Placeholder,
+	upload: Placeholder,
+	smile: Placeholder,
+	pin: Placeholder,
+	search_big: Placeholder,
+	add_image: Placeholder,
+	add_video: Placeholder,
+	photocamera1: Placeholder,
+	photocamera2: Placeholder,
+	photocamera_turnaround: Placeholder,
+	photocamera: Placeholder,
+	takephoto: Placeholder,
+	video_cam: Placeholder,
+	video: Placeholder,
+}
+
+export type IconName = keyof typeof icon
