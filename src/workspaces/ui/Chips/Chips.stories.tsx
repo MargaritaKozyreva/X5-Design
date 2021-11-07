630bc3a25c7813c857ea311f2f1689efefe95dec
Nikita.Zheleznov <zheleznov.frontend@gmail.com>
2021-11-07 13:39:39 +0300

    refactor: upd. Chips

--- src/workspaces/ui/Chips/Chips.stories.tsx
+++ src/workspaces/ui/Chips/Chips.stories.tsx
@@ -1,70 +1,69 @@
-import React from 'react';
-import Chips from "./Chips";
-import Card from "../Card";
-import  './Chips.stories.scss'
-import { Grid } from "./components";
+import React from 'react'
+import Chips from './Chips'
+import Card from '../Card'
+import './Chips.stories.scss'
+import { Grid } from './components'
 
 export default {
 	title: 'Example/Chips',
-	component: Chips
-};
+	component: Chips,
+}
 
 const Template = ({ content, icon }) => (
-  <Card className='chips-container'>
-    <Grid className='chips-grid_grey'>
-      {/* Size-L */}
-      <Chips size='l' >{content}</Chips>
-      <Chips size='l' icon={icon} >{content}</Chips>
-      <Chips size='l' icon={icon} rightIcon >{content}</Chips>
-      <Chips size='l' icon={icon} />
+	<Card className="chips-container">
+		<Grid className="chips-grid_grey">
+			{/* Size-L */}
+			<Chips size="l" value={content} />
+			<Chips size="l" icon={icon} value={content} />
+			<Chips size="l" icon={icon} reverse value={content} />
+			<Chips size="l" icon={icon} />
 
-      <Chips disabled size='l' >{content}</Chips>
-      <Chips disabled size='l' icon={icon} >{content}</Chips>
-      <Chips disabled size='l' icon={icon} rightIcon >{content}</Chips>
-      <Chips disabled size='l' icon={icon} />
+			<Chips disabled size="l" value={content} />
+			<Chips disabled size="l" icon={icon} value={content} />
+			<Chips disabled size="l" icon={icon} reverse value={content} />
+			<Chips disabled size="l" icon={icon} />
 
-      {/* Size-S */}
-      <Chips size='s' >{content}</Chips>
-      <Chips size='s' icon={icon} >{content}</Chips>
-      <Chips size='s' icon={icon} rightIcon >{content}</Chips>
-      <Chips size='s' icon={icon} />
+			{/* Size-S */}
+			<Chips size="s">{content}</Chips>
+			<Chips size="s" icon={icon} value={content} />
+			<Chips size="s" icon={icon} reverse value={content} />
+			<Chips size="s" icon={icon} />
 
-      <Chips disabled size='s' >{content}</Chips>
-      <Chips disabled size='s' icon={icon} >{content}</Chips>
-      <Chips disabled size='s' icon={icon} rightIcon >{content}</Chips>
-      <Chips disabled size='s' icon={icon} />
+			<Chips disabled size="s" value={content} />
+			<Chips disabled size="s" icon={icon} value={content} />
+			<Chips disabled size="s" icon={icon} reverse value={content} />
+			<Chips disabled size="s" icon={icon} />
+		</Grid>
 
-    </Grid>
+		<Grid className="chips-grid_white">
+			{/* Size-L */}
+			<Chips size="l">{content}</Chips>
+			<Chips size="l" icon={icon} />
+			<Chips size="l" icon={icon} reverse />
+			<Chips size="l" icon={icon} />
 
-    <Grid className='chips-grid_white'>
-      {/* Size-L */}
-      <Chips size='l' >{content}</Chips>
-      <Chips size='l' icon={icon} >{content}</Chips>
-      <Chips size='l' icon={icon} rightIcon >{content}</Chips>
-      <Chips size='l' icon={icon} />
+			<Chips disabled size="l" />
+			<Chips disabled size="l" icon={icon} />
+			<Chips disabled size="l" icon={icon} reverse />
+			<Chips disabled size="l" icon={icon} />
 
-      <Chips disabled size='l' >{content}</Chips>
-      <Chips disabled size='l' icon={icon} >{content}</Chips>
-      <Chips disabled size='l' icon={icon} rightIcon >{content}</Chips>
-      <Chips disabled size='l' icon={icon} />
+			{/* Size-S */}
+			<Chips size="s">{content}</Chips>
+			<Chips size="s" icon={icon} />
+			<Chips size="s" icon={icon} reverse />
+			<Chips size="s" icon={icon} />
 
-      {/* Size-S */}
-      <Chips size='s' >{content}</Chips>
-      <Chips size='s' icon={icon} >{content}</Chips>
-      <Chips size='s' icon={icon} rightIcon >{content}</Chips>
-      <Chips size='s' icon={icon} />
+			<Chips disabled size="s" />
+			<Chips disabled size="s" icon={icon} />
+			<Chips disabled size="s" icon={icon} reverse />
+			<Chips disabled size="s" icon={icon} />
+		</Grid>
+	</Card>
+)
 
-      <Chips disabled size='s' >{content}</Chips>
-      <Chips disabled size='s' icon={icon} >{content}</Chips>
-      <Chips disabled size='s' icon={icon} rightIcon >{content}</Chips>
-      <Chips disabled size='s' icon={icon} />
-    </Grid>
-  </Card>
-);
-
-export const all = Template.bind({});
+export const all = Template.bind({})
 
 all.args = {
-  content: 'Chips', 
-  icon: 'loader (not working)'
-};
+	content: 'Chips',
+	icon: 'loader (not working)',
+}
--- src/workspaces/ui/Chips/Chips.tsx
+++ src/workspaces/ui/Chips/Chips.tsx
@@ -1,40 +1,55 @@
 import React, { ButtonHTMLAttributes, useMemo } from 'react'
-import  './Chips.scss'
+import './Chips.scss'
 import cn from 'classnames'
-import Icon, { IconProps } from '../Icon';
-
+import Icon, { IconProps } from '../Icon'
 
 export type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
-  size?: 's' | 'l' 
-  icon?: IconProps['type']
-  children?: string
-  value?: string
-  rightIcon?: boolean
-  active?: boolean
+	size?: 's' | 'l'
+	icon?: IconProps['type']
+	children?: string
+	value?: string
+	reverse?: boolean
+	active?: boolean
 }
 
-export default ({ children, size, value, icon, rightIcon, className, active, ...props }: Props) => {
-  const styles = useMemo(() => ({
-    container: cn('chips', { 
-      'chips_size_s': size !== 'l',  
-      'chips_size_l': size === 'l',  
-      'chips_reverse': !!rightIcon,
-      'chips_with-text': !!children || !!value,  
-     }, className), 
-    icon: cn('chips__icon', {
-      'chips__icon_left': !rightIcon,
-      'chips__icon_right': !!rightIcon,
-    }), 
-    text: cn('chips__text'), 
-  }), [children, value, size, rightIcon, className, active])
+export default function Chips({
+	children,
+	size,
+	value,
+	icon,
+	reverse,
+	className,
+	active,
+	...props
+}: Props) {
+	const styles = useMemo(
+		() => ({
+			container: cn(
+				BASE,
+				`${BASE}_size_${byDefaulSize(size)}`,
+				{
+					[`${BASE}_reverse`]: !!reverse,
+					[`${BASE}_with-text`]: !!children || !!value,
+				},
+				className,
+			),
+			icon: cn(ICON, { [`${ICON}_reverse`]: reverse }),
+			text: cn(TEXT),
+		}),
+		[children, value, size, reverse, className],
+	)
 
 	return (
-    <button {...props} className={styles.container} >
-      {icon && <Icon size='s'  type={icon} className={styles.icon} />}
-      <span className={styles.text}  >{value || children}</span>
-    </button>
-	);
-};
+		<button {...props} className={styles.container}>
+			{icon && <Icon size="s" type={icon} className={styles.icon} />}
+			<span className={styles.text}>{value || children}</span>
+		</button>
+	)
+}
 
+const BASE = 'x5-design-chips'
+const ICON = `${BASE}__icon`
+const TEXT = `${BASE}__text`
 
-
+const byDefaulSize = (size: Props['size'] = 'l') =>
+	['s', 'l'].includes(size) ? size : 'l'
